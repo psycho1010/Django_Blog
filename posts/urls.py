1b3b608086364b4c9a73733ffc9a254d9c6bd754
Himanshu <nitropsycho84@gmai.com>
2025-02-07 13:34:25 +0530

    Initial commit - Django Blog CRUD App

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import post_list, create_post #import the view function
+from .views import post_list, create_post, edit_post, delete_post  #import the view function
 
 urlpatterns = [
     path('', post_list, name='post_list'), #add the URL pattern
     path('new/', create_post, name='create_post'),
+    path('<int:post_id>/edit/', edit_post, name='edit_post'),
+    path('<int:post_id>/delete/', delete_post, name='delete_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 
-# Create your views here.
 from .models import Post #import post model
 from .forms import PostForm #import post form
+
+
 
 def post_list(request):
     posts = Post.objects.all() #fetch all post from the database
     return render(request, 'posts/post_list.html', {'posts': posts}) #pass the posts to the template
+
+
 
 def create_post(request):
     if request.method == 'POST' : #check if the form is submitted
@@ -18,3 +21,28 @@ def create_post(request):
         form = PostForm() #create an empty form
 
     return render(request, 'posts/create_post.html', {'form': form}) #show form
+
+
+
+def edit_post(request, post_id):
+
+    post = get_object_or_404(Post, id=post_id)  # Fetch post or return 404 if not found
+
+    if request.method == 'POST':  
+        form = PostForm(request.POST, instance=post)  # Bind form to existing post
+        if form.is_valid():  
+            form.save()  # Save updated post
+            return redirect('post_list')  # Redirect to post list
+    else:
+        form = PostForm(instance=post)  # Pre-fill form with existing data
+
+    return render(request, 'posts/edit_post.html', {'form': form, 'post': post})
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'posts/delete_post.html', {'post': post})
